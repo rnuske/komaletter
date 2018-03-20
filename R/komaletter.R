c7dbe875a7a12aad6d1e4539d48243ff125d1faf
Robert Nuske <robert.nuske@nw-fva.de>
2018-03-20 17:09:21 +0100

    initial commit

---
+++ R/komaletter.R
@@ -0,0 +1,112 @@
+#' KOMA-Script LaTeX Letter Format
+#'
+#' A format for LaTeX letters via R Markdown using the KOMA-Script class 'scrlttr2'.
+#' It provides layouts for many different window envelope types (German, US,
+#' French, Japanese, ...) and the possibility to define your own.
+#'
+#' @inheritParams rmarkdown::pdf_document
+#' @param ... Additional arguments to \code{\link[rmarkdown:pdf_document]{pdf_document}}
+#' @param keep_tex A boolean toggle to select whether intermediate
+#' LaTeX files are to be kept, defaults to \code{FALSE}
+#' @return R Markdown output format to pass to
+#' \code{\link[rmarkdown:render]{render}}
+#'
+#' @examples
+#' \dontrun{
+#' rmarkdown::draft("MyLetter.Rmd", template="pdf", package="komaletter")
+#' rmarkdown::render("MyLetter.Rmd")
+#' }
+#'
+#' @section Letter features:
+#' Various aspects of the letter can be customized by setting the following
+#' variables in the R Markdown document metadata (aka YAML frontmatter).
+#'
+#' Most commonly used variables:
+#' \tabular{ll}{
+#'   \strong{Variable} \tab \strong{Description}\cr
+#'   author            \tab Writer of the letter\cr
+#'   return-address    \tab Address of the sender; takes a list for a multi-line address.\cr
+#'   address           \tab Name and address of the recipient; takes a list.\cr
+#'   date              \tab Custom date; if not specified, current date will be inserted.\cr
+#'   subject           \tab Subject line.\cr
+#'   opening           \tab Text for the salutation.\cr
+#'   closing           \tab Text for the complementary close.\cr
+#'   signature         \tab Typed signature or image placed below closing.\cr
+#'   signature-before  \tab Allows adjustment of vertical space before signature.
+#' }
+#'
+#' Further variables defined by komaletter:
+#' \tabular{ll}{
+#'   \strong{Variable} \tab \strong{Description}\cr
+#'   lco          \tab Letter Class Option File. Either name of a standard KOMA-Script LCO (e.g. DIN, SN, ) or path to custom LCO. If not specified, the \code{maintainers_delight.lco} will be used.\cr
+#'   lang         \tab Language code according to \href{https://tools.ietf.org/html/bcp47}{BCP 47} (e.g. \code{en} or \code{en-GB}).\cr
+#'   papersize    \tab Size of paper eg. `a4`, `letter`.\cr
+#'   return-phone \tab Phone number of sender used in letter head.\cr
+#'   return-email \tab Email address of sender used in letter head.\cr
+#'   return-url   \tab Website of sender used in letter head.\cr
+#'   place        \tab Sender’s place used near date.\cr
+#'   yourref      \tab Addressee’s reference as part of reference line.\cr
+#'   yourmail     \tab Date of addressee’s referenced mail as part of reference line.\cr
+#'   myref        \tab Sender’s reference as part of reference line.\cr
+#'   customer     \tab Customer number as part of reference line.\cr
+#'   invoice      \tab Invoice number as part of reference line.\cr
+#'   cc           \tab Recipients to be carbon-copied; can take a list for multiple recipients.\cr
+#'   encl         \tab List of enclosures.\cr
+#'   ps           \tab Text to be added at the end of the letter as a postscript.\cr
+#'   komaoption   \tab Specify further KOMA options; takes a list (see KOMA-Script documentation).\cr
+#'   parskip      \tab Defines how to mark new paragraphs, e.g. false, full, half (see KOMA-Script documentation)
+#' }
+#'
+#'
+#' The vignette source shows several of these options in use.
+#'
+#' @seealso
+#' \code{\link[linl]{linl}}
+#'
+#' @references
+#' JJ Allaire, R Foundation, Hadley Wickham, Journal of Statistical Software, Yihui Xie, Ramnath
+#' Vaidyanathan, Association for Computing Machinery, Carl Boettiger, Elsevier, Karl Broman,
+#' Kirill Mueller, Bastiaan Quast, Randall Pruim, Ben Marwick, Charlotte Wickham, Oliver Keyes
+#' and Miao Yu (2017). rticles: Article Formats for R Markdown. R package version 0.4.1.
+#' \url{https://cran.r-project.org/package=rticles}
+#'
+#' JJ Allaire, Joe Cheng, Yihui Xie, Jonathan McPherson, Winston Chang, Jeff Allen, Hadley
+#' Wickham, Aron Atkins, Rob Hyndman and Ruben Arslan (2017). rmarkdown: Dynamic Documents for R.
+#' R package version 1.6. \url{https://cran.r-project.org/package=rmarkdown}
+#'
+#' Yihui Xie (2017). knitr: A General-Purpose Package for Dynamic Report Generation in R. R
+#' package version 1.17.
+#'
+#' Aaron Wolen (2017). pandoc-letter. GitHub Repository. \url{https://github.com/aaronwolen/pandoc-letter}.
+#'
+#' Dirk Eddelbuettel and Aaron Wolen (2017). linl: 'linl' is not 'Letter'. R package version 0.0.2. \url{https://cran.r-project.org/package=linl}
+#'
+#' @export
+komaletter <- function(..., keep_tex=FALSE){
+
+  template <- system.file("rmarkdown", "templates", "pdf", "resources",
+                          "template.tex", package="komaletter")
+  path_to_default <- system.file("rmarkdown", "templates", "pdf", "resources",
+                                 "maintainers_delight.lco", package="komaletter")
+  path_to_default <- sub("\\.[^.]*$", "", path_to_default)
+
+  base <- inherit_pdf_document(..., template=template, keep_tex=keep_tex,
+                               md_extensions=c("-autolink_bare_uris"),
+                               pandoc_args=c(paste0("--variable=lco_default:",
+                                                  path_to_default)))
+
+  base$knitr$opts_chunk$prompt    <- FALSE  # changed from TRUE
+  base$knitr$opts_chunk$comment   <- '# '   # default to one hashmark
+  base$knitr$opts_chunk$highlight <- TRUE   # changed as well
+
+  return(base)
+}
+
+
+# Call rmarkdown::pdf_document and mark the return value as inheriting pdf_document
+inherit_pdf_document <- function(...){
+  fmt <- rmarkdown::pdf_document(...)
+  fmt$inherits <- "pdf_document"
+
+  return(fmt)
+}
---
+++ README.md
@@ -0,0 +1,83 @@
+
+Komaletter: simple yet flexible Letters via the KOMA-Script LaTeX Bundle 
+
+
+### Motivation
+Simple letters tend to look very much alike. They are either determined by a 
+standard eg. window envelope style or writer's preferences. If the layout is fixed,
+they lend themselves to be written in R Markdown.
+
+The [KOMA-Script LaTeX Bundle](https://www.komascript.de) provides layouts for
+many common window envelope types (German, US, French, Japanese, ...) and the
+possibility to define your own. *komaletter* also provides it's own default
+layout loosely based on DIN 5008B in the so-called Letter Class Option file
+'maintainers_delight.lco'  
+
+This package is an adaptation of [linl](https://cran.r-project.org/package=rmarkdown) 
+by Dirk Eddelbuettel and Aaron Wolen which in turn leans on earlier work by 
+Aaron Wolen in his [pandoc-letter](https://github.com/aaronwolen/pandoc-letter) 
+repository, and extends it for use from R via the
+[rmarkdown](https://cran.r-project.org/package=rmarkdown) package.
+
+
+### Example
+
+#### Skeleton
+The skeleton creates a very simple letter.  This shows the (complete) source 
+on the left and the rendered pdf on the right:
+
+![](./docs/skeleton.png)
+
+Several formatting defaults for font, fontsize, indentation are in use. See 
+`help(komaletter)` for a complete list and default values.
+
+#### Vignette
+The vignette example is a little more featureful and shows how to include a 
+signature and a few formatting settings. All of these are driven by simple 
+YAML headers as seen on the left:
+
+![](./docs/letter_example1.png)
+
+The vignette also contains a few lines of vignette metadata one would be 
+excluded from a normal letter.
+
+
+### Status
+The package works as intended but still needs some testing.
+
+
+### Usage 
+Since the package is not yet on CRAN, the best guess is to use devtools
+
+```r
+devtools::install_github("rnuske/komaletter")
+```
+
+then use as a Markdown template via RStudio, or use code such as
+
+```r
+library(rmarkdown)
+draft("myletter.Rmd", template="pdf", package="komaletter", edit=FALSE)
+render("myletter.Rmd")
+```
+
+to create a first draft of a new `myletter.Rmd`.
+
+
+### Requirements
+Beyond the R package dependencies, a working `pandoc` binary and a LaTeX 
+distribution including KOMA-Script is needed. RStudio installs its own copy of 
+pandoc, otherwise do what is needed on your OS (_i.e._, something like `sudo 
+apt-get install pandoc pandoc-citeproc`). For LaTeX look for `texlive` which is
+included in most Linux distributions or `MiKTeX` if you are using Windows. 
+KOMA-Script is part of all but the most bare bone LaTeX distribution. Something
+like `sudo apt-get install texlive` should provide everything needed.
+
+
+### Authors
+Robert Nuske, Dirk Eddelbuettel and Aaron Wolen.
+
+
+### License
+GPL-3 for this package, the work in [pandoc-letter](https://github.com/aaronwolen/pandoc-letter), 
+as well as underlying Pandoc template.
